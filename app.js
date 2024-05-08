fc3a85c5a460f294cbf7dd2c3b922297c92a6eff
juleshb <habaruremajules@gmail.com>
2024-05-08 18:49:04 +0200

    adding cors anf fix data submition

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-// app.js
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const multer = require('multer');
 
 const app = express();
 const PORT = process.env.PORT || 4700;
 
 // Middleware
+app.use(cors()); // Enable CORS
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(multer().none()); // Parse multipart/form-data
+
+// Log the request body
+app.use((req, res, next) => {
+  console.log('Request Body:', req.body);
+  next();
+});
 
 // Nodemailer setup
 const transporter = nodemailer.createTransport({
