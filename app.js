9dcedb90166fd2f79ca7691bc1c7b8cd3b82d7f4
juleshb <habaruremajules@gmail.com>
2024-05-10 14:25:34 +0200

    add end point

--- app.js
+++ app.js
@@ -96,6 +96,71 @@ transporter.sendMail(mailOptions2, (error, info) => {
 
 });
 
+app.post('/contact', (req, res) => {
+  const { names, emails, message } = req.body;
+
+  // Email content
+  const mailOptions = {
+    from: 'hostelemmaus@gmail.com',
+    to: 'habaruremajules@gmail.com',
+    subject: 'Contact us message',
+    html: `
+      <h2>Contact Form Submission</h2>
+      <p><strong>Name:</strong> ${names}</p>
+      <p><strong>Email:</strong> ${emails}</p>
+      <p><strong>Phone:</strong> ${message}</p>
+      
+    `
+  };
+
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log('Error occurred:', error);
+      res.status(500).send('Error occurred, form submission failed.');
+    } else {
+      console.log('Email sent:', info.response);
+      res.status(200).send('Form submitted successfully.');
+    }
+  });
+
+ // Email content
+ const mailOptions2 = {
+  from: 'hostelemmaus@gmail.com',
+  to: emails,
+  subject: 'Contact form Submission',
+  html: `
+    <h4>Contact form Submission</h4>
+    <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f5f5f5;">
+        <img src="https://emmaus.vercel.app/static/media/logo.caae3e16f2670b31435d.png" alt="Banner Image" style="width: 100%; max-width: 600px;">
+        <h2 style="color: #333333;"> Emmaus Hostel</h2>
+        <hr>
+        <p>Dear ${names},</p>
+        <p>Thank you for reaching out to us! We have received your email and appreciate you taking the time to contact us.</p>
+        <p>We are currently reviewing your message and will get back to you as soon as possible. Our team aims to respond within 3h during regular business hours 24/24.</p>
+        <p>In the meantime, if you have any urgent concerns or additional information to share, please feel free to reach out to us directly at [your contact information].</p>
+        <p>Thank you for your patience and interest in Emmaus Hostel. We look forward to assisting you!</p>
+        <br>
+        <p>Best regards,</p>
+        <p>Habarurema jules<br>Customacare<br>Emmaus Hostel<br>+25089028283</p>
+    </div>
+  `
+};
+
+
+// Send email
+transporter.sendMail(mailOptions2, (error, info) => {
+  if (error) {
+    console.log('Error occurred:', error);
+    res.status(500).send('Error occurred, form submission failed.');
+  } else {
+    console.log('Email sent:', info.response);
+    res.status(200).send('Form submitted successfully.');
+  }
+});
+
+
+});
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
