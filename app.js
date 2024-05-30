f6fff5817139224068135f12d6ca9f1bbd3dd779
juleshb <habaruremajules@gmail.com>
2024-05-30 15:11:44 +0200

    update message

--- app.js
+++ app.js
@@ -68,17 +68,32 @@ app.post('/submit-form', (req, res) => {
   to: email,
   subject: 'Your reservation has well received',
   html: `
-    <p>Your reservation has well received. We will get back to you soon for confirming your reservation. Thanks for booking with us!</p>
     <h4>Booking  Submission</h4>
-    <p><strong>Name:</strong> ${name}</p>
-    <p><strong>Email:</strong> ${email}</p>
-    <p><strong>Phone:</strong> ${phone}</p>
-    <p><strong>Check-in Date:</strong> ${checkInDate}</p>
-    <p><strong>Check-out Date:</strong> ${checkOutDate}</p>
-    <p><strong>No of Rooms:</strong> ${roomNumber}</p>
-    <p><strong>Room Category:</strong> ${category}</p>
-    <p><strong>Country:</strong> ${country}</p>
-    <p><strong>Other Request:</strong> ${otherRequest}</p>
+    <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f5f5f5;">
+        <img src="https://emmaus.vercel.app/static/media/logo.caae3e16f2670b31435d.png" alt="Banner Image" style="width: 100%; max-width: 600px;">
+        <h2 style="color: #333333;"> Emmaus Hostel</h2>
+        <hr>
+        <p>Dear ${name},</p>
+        <p>Your reservation has well received. We will get back to you soon for confirming your reservation. Thanks for booking with us!</p>
+        <p>We are currently reviewing your message and will get back to you as soon as possible. Our team aims to respond within 3h during regular business hours 24/24.</p>
+        <p>In the meantime, if you have any urgent concerns or additional information to share, please feel free to reach out to us directly at +250 787 172 920 .</p>
+        <p>Thank you for your patience and interest in Emmaus Hostel. We look forward to assisting you!</p>
+        <br>
+        <p>Best regards,</p>
+        <p>Emmaus Hostel<br>+250 787 172 920</p>
+
+        <h4>Submission info:</h4>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone:</strong> ${phone}</p>
+        <p><strong>Check-in Date:</strong> ${checkInDate}</p>
+        <p><strong>Check-out Date:</strong> ${checkOutDate}</p>
+        <p><strong>No of Rooms:</strong> ${roomNumber}</p>
+        <p><strong>Room Category:</strong> ${category}</p>
+        <p><strong>Country:</strong> ${country}</p>
+        <p><strong>Other Request:</strong> ${otherRequest}</p>
+    
+    </div>
   `
 };
 
@@ -133,14 +148,29 @@ app.post('/submit-restorent', (req, res) => {
   to: email,
   subject: 'Your Order has well received',
   html: `
-    <p>Your Order has well received. We will get back to you soon for confirming your reservation. Thanks for booking with us!</p>
+
     <h4>New Order Submission</h4>
-    <p><strong>Name:</strong> ${name}</p>
-    <p><strong>Email:</strong> ${email}</p>
-    <p><strong>Phone:</strong> ${phone}</p>
-    <p><strong>Check-in Date:</strong> ${checkInDate}</p>
-    <p><strong>Order type:</strong> ${category}</p>
-    <p><strong>Other Request:</strong> ${otherRequest}</p>
+    <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f5f5f5;">
+        <img src="https://emmaus.vercel.app/static/media/logo.caae3e16f2670b31435d.png" alt="Banner Image" style="width: 100%; max-width: 600px;">
+        <h2 style="color: #333333;"> Emmaus Hostel</h2>
+        <hr>
+        <p>Dear ${name},</p>
+        <p>Your Order has well received. We will get back to you soon for confirming your reservation. Thanks for booking with us!</p>
+        <p>We are currently reviewing your message and will get back to you as soon as possible. Our team aims to respond within 3h during regular business hours 24/24.</p>
+        <p>In the meantime, if you have any urgent concerns or additional information to share, please feel free to reach out to us directly at +250 787 172 920 .</p>
+        <p>Thank you for your patience and interest in Emmaus Hostel. We look forward to assisting you!</p>
+        <br>
+        <p>Best regards,</p>
+        <p>Emmaus Hostel<br>+250 787 172 920</p>
+
+        <h4>Submission info:</h4>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone:</strong> ${phone}</p>
+        <p><strong>Check-in Date:</strong> ${checkInDate}</p>
+        <p><strong>Order type:</strong> ${category}</p>
+        <p><strong>Other Request:</strong> ${otherRequest}</p>
+    </div>
   `
 };
 
@@ -195,8 +225,21 @@ app.post('/submit-hall', (req, res) => {
   to: email,
   subject: 'Your Booking has well received',
   html: `
-    <p>Your Booking has well received. We will get back to you soon for confirming your reservation. Thanks for booking with us!</p>
     <h4>New Order Submission</h4>
+    <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f5f5f5;">
+        <img src="https://emmaus.vercel.app/static/media/logo.caae3e16f2670b31435d.png" alt="Banner Image" style="width: 100%; max-width: 600px;">
+        <h2 style="color: #333333;"> Emmaus Hostel</h2>
+        <hr>
+        <p>Dear ${name},</p>
+        <p>Your Booking has well received. We will get back to you soon for confirming your reservation. Thanks for booking with us!</p>
+        <p>We are currently reviewing your message and will get back to you as soon as possible. Our team aims to respond within 3h during regular business hours 24/24.</p>
+        <p>In the meantime, if you have any urgent concerns or additional information to share, please feel free to reach out to us directly at +250 787 172 920 .</p>
+        <p>Thank you for your patience and interest in Emmaus Hostel. We look forward to assisting you!</p>
+        <br>
+        <p>Best regards,</p>
+        <p>Emmaus Hostel<br>+250 787 172 920</p>
+
+        <h4>Submission info:</h4>
     <p><strong>Name:</strong> ${name}</p>
     <p><strong>Email:</strong> ${email}</p>
     <p><strong>Phone:</strong> ${phone}</p>
@@ -204,6 +247,7 @@ app.post('/submit-hall', (req, res) => {
     <p><strong>Check-out Date:</strong> ${checkOutDate}</p>
     <p><strong>Conferance type:</strong> ${category}</p>
     <p><strong>Other Request:</strong> ${otherRequest}</p>
+    </div>
   `
 };
 
@@ -263,11 +307,11 @@ app.post('/contact', (req, res) => {
         <p>Dear ${names},</p>
         <p>Thank you for reaching out to us! We have received your email and appreciate you taking the time to contact us.</p>
         <p>We are currently reviewing your message and will get back to you as soon as possible. Our team aims to respond within 3h during regular business hours 24/24.</p>
-        <p>In the meantime, if you have any urgent concerns or additional information to share, please feel free to reach out to us directly at [your contact information].</p>
+        <p>In the meantime, if you have any urgent concerns or additional information to share, please feel free to reach out to us directly at +250 787 172 920 .</p>
         <p>Thank you for your patience and interest in Emmaus Hostel. We look forward to assisting you!</p>
         <br>
         <p>Best regards,</p>
-        <p>Habarurema jules<br>Customacare<br>Emmaus Hostel<br>+25089028283</p>
+        <p>Emmaus Hostel<br>+250 787 172 920</p>
     </div>
   `
 };
